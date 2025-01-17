4df2c6847305b356bb2a4b51b6be8e048c9b81ab
Kauan Fonseca <131304556+COWANBAS@users.noreply.github.com>
2025-01-17 18:03:19 -0300

    Add files via upload

---
+++ Reload.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name           Recarregar Páginas
+// @description    Recarrega páginas assim que voltam para o foco
+// @namespace      CowanPAGE
+// @license        CowBas
+// @version        1.0
+// @author         Cowanbas
+// @match          *://*/*
+// @exclude        *://*.youtube.com/*
+// @exclude        https://web.whatsapp.com/*
+// @run-at         document-start
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // Adiciona um evento de foco à janela
+  window.addEventListener('focus', function () {
+    // Recarrega a página quando a janela ganha foco
+    location.reload();
+  });
+})();
